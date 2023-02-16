1163bd9bdb3ee8a97871fd5e7b55cfca712e07f4
Altay Turan <032090011@ogr.uludag.edu.tr>
2023-02-16 20:54:09 +0300

    10

--- index/index.js
+++ index/index.js
@@ -115,26 +115,6 @@ fetch("../data/links.json")
     }
   });
 
-fetch("../data/gamelinks.json")
-  .then((response) => response.json())
-  .then((json) => {
-    let linksDiv = document.querySelector("div.gamelinks");
-    for (let l of json.games) {
-      if(linksDiv.id.toString() == l.gameName.toString()){
-        for(let k of l.links){
-          linksDiv.insertAdjacentHTML(
-            "beforeend",
-            `<a href="${k.link}" target="_blank" class="link" style="--hc: ${k.color};">
-            <div>
-              <h3 class="name">${k.name}</h3>
-            </div>
-            <img src="./assets/icons/${k.fileName}" alt="">
-          </a>`
-          );
-        }
-      }
-    }
-  });
 
 window.onload = function typewriterEffect() {
   const titles = [
